2c4a17c1023995dc936d71e925146be864cdf2a7
Graham Patterson <gpp2109@columbia.edu>
2018-09-08 12:10:13 -0400

    Adds assignemnt distributing
    make_roster.py takes a roster from tha Grades tab on Courseworks, then
    parses it to a student roster. assign_tas.py will distribute the
    ta's across all the students.
    
    Head TA's will grade about 50% of what the TA's grade
    
    The workflow for this running assign_ta.py, which will output a csv.

---
+++ grading-setup/make_roster.py
@@ -0,0 +1,54 @@
+'''
+    Written by Graham Patterson
+    COMS W1004
+'''
+import argparse, csv, sys
+
+def parse_roster(filename):
+    try:
+        with open(filename, 'r') as f:
+            reader = csv.DictReader(f)
+            data = list()
+            for row in reader:
+                to_add = {
+                          'name':row['Student'].strip('"'),
+                          'uni':row['SIS User ID'],
+                          'id':row['ID']
+                          }
+                if to_add['id']:
+                    data.append(to_add)
+            return data
+    except OSError:
+        return False
+
+def read_csv(filename):
+    try:
+        with open(filename, 'r') as f:
+            reader = csv.DictReader(f)
+            data = list()
+            for row in reader:
+                data.append(row)
+            return data
+    except OSError:
+        return False
+
+def write_csv(filename, data):
+    with open(filename, 'w') as f:
+        writer = csv.DictWriter(f, fieldnames=data[0].keys())
+        writer.writeheader()
+        for item in data:
+            writer.writerow(item)
+
+if __name__ == "__main__":
+    parse = argparse.ArgumentParser(description="""
+                                    Parses the student roster downloaded from
+                                    courseworks under the Grades tab
+                                    """, add_help=True, prog="make-roster.py")
+    parse.add_argument("-r", "--roster", required=True, dest="student_roster",
+                       help="File downloaded from courseworks Grades tab")
+    args=vars(parse.parse_args())
+    roster=parse_roster(args['student_roster'])
+    if not roster:
+        sys.stderr.write("Error {} does not exist\n".format(args['student_roster']))
+        sys.exit(-1)
+    write_csv("parsed-roster.csv", roster)
