46f5f6124e84b4b1465a11caedd8120e94374bd0
Graham Patterson <pattersongp.usmc@gmail.com>
2018-09-25 20:42:11 -0400

    update postgrades -- pls dont use my apikey

--- postgrades.py
+++ postgrades.py
@@ -10,9 +10,10 @@ import sys
 API_URL = "https://courseworks2.columbia.edu"
 
 # Canvas API key
-API_KEY = "YOU MUST FILL THIS IN"
-COURSE_ID_SECTION1 = "YOU MUST FILL THIS IN"
-ASSIGNMENT_ID_SECTION1 = '-1'
+API_KEY = "1396~IRD9JnuAmX2mush0297wIRxhGOqREhYkqntq2nSo4vkG0mzrLyRDIKq9O9X6O3E5"
+
+COURSE_ID_SECTION1 = "59635"
+ASSIGNMENT_ID_SECTION1 = ""
 canvas = Canvas(API_URL, API_KEY)
 
 """ Takes in a csv sheet of grades (go to the excel sheet, File -> Download as --> .csv) that
@@ -21,29 +22,26 @@ Namely, name, uni, canvas id, section number, total score, and comments """
 def process_grading_sheet(grades, push_grade):
     rows = grades.index
     column_headers = list(grades)
+    late_index = column_headers.index("ta")
 
     # to show students grade for each question, grade columns need to be between
     # these two column names
-    late_index = column_headers.index("Late (TRUE/FALSE)")
     comment_index = column_headers.index("Comments")
     homework_questions_list = column_headers[late_index + 1: comment_index]
 
     for i in rows:
         row_data = grades.iloc[[i]]
-        name = grades['Name'][i]
-        canvas_id = grades['ID'][i]
+        name = grades['name'][i]
+        canvas_id = grades['id'][i]
         total_score = grades['Total'][i]
-        section = grades['Section'][i]
-        isLate = grades['Late (TRUE/FALSE)'][i]
-        isLate_comment = "This assignment was turned in late (20% penalty if true): " + str(isLate)
         comments = grades['Comments'][i]
         if pd.isnull(comments):
             comments = "No comments from TAs."
-        comments = str(isLate_comment) + "\n" + str(comments)
+        comments = "\n{}\n".format(comments)
         comment_message = make_comment(row_data, homework_questions_list, comments)
         if push_grade:
             try:
-                post_grade(canvas_id, total_score, comment_message, section)
+                post_grade(canvas_id, total_score, comment_message)
                 print("Pushed grade for " + name)
             except Exception as e:
                 print(e)
@@ -51,7 +49,6 @@ def process_grading_sheet(grades, push_grade):
         else:
             print("NAME: " + name)
             print("CANVAS ID: " + str(canvas_id))
-            print("SECTION: " + str(section))
             print("TOTAL SCORE: " + str(total_score))
             print(comment_message)
             print("--------------------")
@@ -62,11 +59,11 @@ def make_comment(row_data, homework_questions, base_comment):
         score = str(homework_question) + ": " + str(row_data[homework_question].values.flatten()[0])
         comment += score + " | "
 
-    comment += """\nTo request a regrade, see the corresponding Canvas
-    announcement detailing which TA graded that particular question."""
+    comment += "\n\nAny errors in grading need to be resolved within 1 week of this posting."
+
     return comment
 
-def post_grade(canvas_id, total_score, comments, section):
+def post_grade(canvas_id, total_score, comments):
     #assume there's only two sections
     course = canvas.get_course(COURSE_ID_SECTION1)
     submissions = course.list_submissions(ASSIGNMENT_ID_SECTION1)
@@ -77,7 +74,6 @@ def post_grade(canvas_id, total_score, comments, section):
     #
     grade = course.update_submission(ASSIGNMENT_ID_SECTION1,canvas_id,
     comment={'text_comment': comments}, submission={'posted_grade': total_score} )
-    #if not section 1, must be section 2
     return grade
 
 def _boolean_(string):
